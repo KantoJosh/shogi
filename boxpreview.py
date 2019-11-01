9aa191af15160ab20c4bff554b9f6ea692513a26
isg99 <igessess@uci.edu>
2019-10-31 23:22:41 -0700

    Fixed possible moves for preview

--- boxpreview.py
+++ boxpreview.py
@@ -1,13 +1,19 @@
 from piece import Piece 
-
+from utils import LOWER,UPPER,BOARD_SIZE
+# Checklist
+# possible moves work
 class BoxPreview(Piece):
     def __init__(self,row,column,player):
         Piece.__init__(self,row,column,player)
     
     def possibleMoves(self,board): 
-        offset = 1 if self.player == 0 else -1
-        if (4 >= (self.position[0] + offset) >= 0) and (4 >= (self.position[1]) >= 0):
-            return [[self.position[0] + offset,self.position[1]]]
+        x,y = self.position
+        offset = 1 if self.player == LOWER else -1
+        if (BOARD_SIZE > (x + offset) >= 0) and \
+        (BOARD_SIZE >= (y) >= 0) and \
+        (board[(x + offset,y)] == None or board[(x + offset,y)].player != self.player):
+            print([x+offset,y])
+            return [[x + offset,y]]
         else:
             return []
     
