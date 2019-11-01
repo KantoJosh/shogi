c4f22eb3c636ad2c00eb149dd6c9481e8edddd23
isg99 <igessess@uci.edu>
2019-10-31 23:09:55 -0700

    Box relay possible moves fixed

--- boxrelay.py
+++ boxrelay.py
@@ -1,5 +1,7 @@
 from piece import Piece 
-
+from utils import BOARD_SIZE
+# Checklist
+# possible moves works
 class BoxRelay(Piece):
     def __init__(self,row,column,player):
         Piece.__init__(self,row,column,player)
@@ -7,13 +9,20 @@ class BoxRelay(Piece):
     def possibleMoves(self,board): 
         offset = 1 if self.player == 0 else -1
         moves = [
+            # diag
             [self.position[0] + 1, self.position[1] + 1],
             [self.position[0] + 1, self.position[1] - 1],
-            [self.position[0] + 1, self.position[1]],
+            # front of it -- only this one needs offset since other points 
+            # dont change their coordinate based on orientation/which player the piece
+            # belongs to
+            [self.position[0] + offset, self.position[1]],
+            # diagonal behind
             [self.position[0] - 1, self.position[1] + 1],
             [self.position[0] - 1, self.position[1] - 1]
         ]
 
-        return list(filter(lambda m: m[0] >= 0 and m[1] < 5,moves))
-    
+        x = list(filter(lambda m: BOARD_SIZE > m[0] >= 0 and  0 <= m[1] < BOARD_SIZE
+        and (board[(m[0],m[1])] == None or board[(m[0],m[1])].player != self.player),moves))
+        print(x)
+        return x
     
